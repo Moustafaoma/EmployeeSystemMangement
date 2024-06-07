2bfc75da2cb0032e0ccf476337d33e17c9b9557a
Mostafa <moustafaomar212@gmail.com>
2024-06-08 02:45:33 +0300

    Add project files.

---
+++ EmployeeSystemMangement.DAL/Data/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeSystemMangement.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystemMangement.DAL.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=EmployeeSystemDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
---
+++ EmployeeSystemMangement.DAL/Data/Configrations/DepartmentCongurations.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeSystemMangement.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystemMangement.DAL.Data.Configrations
+{
+    public class DepartmentCongurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            //Fluent API of Department
+            builder.HasKey(d => d.Id);
+            builder.Property(d => d.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn(10, 10);
+
+            // Configure Name with max length 50 and required
+            builder.Property(d => d.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            // Configure Code with max length 50 and required
+            builder.Property(d => d.Code)
+                .HasMaxLength(50)
+                .IsRequired();
+
+        }
+    }
+}
