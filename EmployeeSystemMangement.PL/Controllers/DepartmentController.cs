88057320c0b3788c41b7f1fe47c718c29fd2588f
Mostafa <moustafaomar212@gmail.com>
2024-06-11 03:36:38 +0300

    AddDepartmentConroller and add Index Action with their view

---
+++ EmployeeSystemMangement.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeSystemMangement.BLL.Interfaces;
+using EmployeeSystemMangement.BLL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace EmployeeSystemMangement.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository ;
+        }
+        public IActionResult Index()
+        {
+            var departments = _departmentRepository.GetAll();
+            if(departments.Count()==0)
+                return NotFound("No Department Found");
+            return View(departments);
+        }
+    }
+}
