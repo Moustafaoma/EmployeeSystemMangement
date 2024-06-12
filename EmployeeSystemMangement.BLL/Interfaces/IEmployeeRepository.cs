dbf1fe0f5f54af2f55a70c9dbd5206cbad7a5528
Mostafa <moustafaomar212@gmail.com>
2024-06-12 21:31:55 +0300

    Add BLL for Emoloyee Module

---
+++ EmployeeSystemMangement.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeSystemMangement.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystemMangement.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee Entity);
+        int Update(Employee Entity);
+        int Delete(Employee Entity);
+    }
+}
---
+++ EmployeeSystemMangement.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeSystemMangement.BLL.Interfaces;
+using EmployeeSystemMangement.DAL.Data;
+using EmployeeSystemMangement.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSystemMangement.BLL.Repositories
+{
+    public class EmployeeRepository:IEmployeeRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public EmployeeRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.AsNoTracking().ToList();
+        }
+        public Employee GetById(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+        public int Add(Employee Entity)
+        {
+            _context.Employees.Add(Entity);
+            return _context.SaveChanges();
+        }
+        public int Update(Employee Entity)
+        {
+            _context.Employees.Update(Entity);
+            return _context.SaveChanges();
+        }
+        public int Delete(Employee Entity)
+        {
+            _context.Remove(Entity);
+            return _context.SaveChanges();
+        }
+
+
+    }
+}
