737705c60d8fba50d236d0fabd4aeade39c8b741
Mostafa <moustafaomar212@gmail.com>
2024-06-27 12:01:19 +0300

    Implement Forget password and reset password by sending link in user gmail

---
+++ EmployeeSystemMangement.PL/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeSystemMangement.DAL.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeSystemMangement.PL.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+		[Required(ErrorMessage = "New Password is required")]
+		[StringLength(100, ErrorMessage = "Password must be at least 6 characters long")]
+		[DataType(DataType.Password)]
+		public string NewPassword { get; set; }
+		[Required(ErrorMessage = "Please confirm your password")]
+		[DataType(DataType.Password)]
+		[Compare(nameof(NewPassword), ErrorMessage = "The New password and confirmation password do not match.")]
+		public string ConfirmedPassword { get; set; }
+
+
+
+	}
+}
